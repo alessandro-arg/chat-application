72feae0f239294d7db010b0113507e4011230634
Alessandro Argenziano <1603alessandro@gmail.com>
2025-05-22 14:14:11 +0200

    the list component got default data and has a decent layout

--- src/components/list/chat-list/Chatlist.jsx
+++ src/components/list/chat-list/Chatlist.jsx
@@ -1,7 +1,66 @@
+import { useState } from "react";
 import "./chat-list.css";
 
 const Chatlist = () => {
-  return <div className="chat-list">Chatlist</div>;
+  const [addMode, setAddMode] = useState(false);
+  return (
+    <div className="chat-list">
+      <div className="search">
+        <div className="search-bar">
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder="Search" />
+        </div>
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chatlist;
