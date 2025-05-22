30957b31c622e41eec497e39671cf05761d44cb5
Alessandro Argenziano <1603alessandro@gmail.com>
2025-05-23 01:45:59 +0200

    working on the css and the html of the detail component

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,7 +1,86 @@
 import "./detail.css";
 
 const Detail = () => {
-  return <div className="detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photo-item">
+              <div className="photo-detail">
+                <img src="./favicon.png" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photo-item">
+              <div className="photo-detail">
+                <img src="./favicon.png" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photo-item">
+              <div className="photo-detail">
+                <img src="./favicon.png" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photo-item">
+              <div className="photo-detail">
+                <img src="./favicon.png" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photo-item">
+              <div className="photo-detail">
+                <img src="./favicon.png" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photo-item">
+              <div className="photo-detail">
+                <img src="./favicon.png" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block user</button>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
