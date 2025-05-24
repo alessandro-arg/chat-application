58de4167ec19c5095cfbaa4559474184e7107269
Alessandro Argenziano <1603alessandro@gmail.com>
2025-05-24 14:05:26 +0200

    implemented and imported the auth, database and storage from firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "react-chat-app-14a4a.firebaseapp.com",
+  projectId: "react-chat-app-14a4a",
+  storageBucket: "react-chat-app-14a4a.firebasestorage.app",
+  messagingSenderId: "483394613614",
+  appId: "1:483394613614:web:bc2e323f980ed5d7e6117f",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+export const db = getFirestore();
+export const storage = getStorage();
