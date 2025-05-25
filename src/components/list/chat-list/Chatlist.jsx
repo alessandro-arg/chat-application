ff445aab0cdc19d35be5f139a452ef7ddf8aa48e
Alessandro Argenziano <1603alessandro@gmail.com>
2025-05-26 00:16:05 +0200

    now the isSeen status is changing when i click on a unreaden chat

--- src/components/list/chat-list/Chatlist.jsx
+++ src/components/list/chat-list/Chatlist.jsx
@@ -3,7 +3,7 @@ import "./chat-list.css";
 import AddUser from "./add-user/AddUser";
 import useUserStore from "../../../lib/user-store";
 import useChatStore from "../../../lib/chat-store";
-import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
 import { db } from "../../../lib/firebase";
 
 const Chatlist = () => {
@@ -39,7 +39,27 @@ const Chatlist = () => {
   }, [currentUser.id]);
 
   const handleSelect = async (chat) => {
-    changeChat(chat.chatId, chat.user);
+    const userChats = chats.map((item) => {
+      const { user, ...rest } = item;
+      return rest;
+    });
+
+    const chatIndex = userChats.findIndex(
+      (item) => item.chatId === chat.chatId
+    );
+
+    userChats[chatIndex].isSeen = true;
+
+    const userChatRef = doc(db, "userchats", currentUser.id);
+
+    try {
+      await updateDoc(userChatRef, {
+        chats: userChats,
+      });
+      changeChat(chat.chatId, chat.user);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -61,6 +81,7 @@ const Chatlist = () => {
           className="item"
           key={chat.chatId}
           onClick={() => handleSelect(chat)}
+          style={{ backgroundColor: chat?.isSeen ? "transparent" : "#5183fe" }}
         >
           <img src={chat.user?.avatar || "./avatar.png"} alt="" />
           <div className="texts">
